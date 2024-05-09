5ba40192ab15e5c3908d814565d3cca8076c4ac2
pooja1232 <pp8927824@gmail.com>
2024-05-09 18:36:25 +0530

    message

---
+++ sketch_may9a/sketch_may9a.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(2, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(2,HIGH);  
+}
+
